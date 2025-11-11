f06b0d6c8529b7985ce563ab148e7264591b75d2
Lucas Lima <lucasarts2020@gmail.com>
2025-11-11 02:55:06 -0300

    att4

--- view/src/pages/login.jsx
+++ view/src/pages/login.jsx
@@ -1,5 +1,5 @@
 import Center from "../components/Center";
-import Imput from "../components/imput";
+import Input from "../components/Input";
 
 function Login() {
   return (
@@ -13,8 +13,8 @@ function Login() {
         Conecte-se com artista, produtores e oportunidades na indústria musical.
       </p>
       <form action="" className="w-full max-w-xl flex flex-col gap-5">
-        <Imput type={"email"} description={"example@gmail.com"}></Imput>
-        <Imput type={"password"} description={"*******"}></Imput>
+        <Input type={"email"} description={"example@gmail.com"}></Input>
+        <Input type={"password"} description={"*******"}></Input>
         <button
           className="w-full mt-6 bg-linear-to-r from-yellow-300 to-pink-400 text-black font-bold py-3 
           rounded-full hover:opacity-50 transition text-2xl"
