1e51c0c9c4231f2723efa339c3748ac66564729a
Danilo Gregorio <Dgg.trab@gmail.com>
2025-11-12 13:51:34 -0300

    criação do server

--- client/src/components/homeComponents/BemVindo.jsx
+++ client/src/components/homeComponents/BemVindo.jsx
@@ -8,11 +8,10 @@ function BemVindo() {
     <Center>
       <div className="flex flex-col lg:flex-row justify-between items-center py-16 px-12 md:px-20">
         <div className="max-w-xl mb-12 lg:mb-0">
-          <h1 className="text-5xl md:text-6xl font-extrabold leading-tight">
-            <Title title={"Conecte talentos"} />
-            <br />
-            <Title title={"Amplifique sons"} />
-          </h1>
+          <Title
+            title={`Conecte talentos
+            Amplifique sons`}
+          />
 
           <p className="mt-6 text-lg text-gray-300">
             Uma plataforma para artistas, bandas, produtores, e fãs se
@@ -20,8 +19,6 @@ function BemVindo() {
           </p>
         </div>
         <img src={banda} className="w-150 h-150 ml-20"></img>
-
-        <div className="w-full lg:w-1/2 flex justify-center"></div>
       </div>
     </Center>
   );
