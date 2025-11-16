4ea16189fe3d13afb02886e8d5cd5b0a5ff4ae9e
Danilo Gregorio <Dgg.trab@gmail.com>
2025-11-16 01:47:49 -0300

    criado todas as tabelas do banco nas pastas dominio

--- api/src/main/java/com/mconnect/api/domain/Avaliacao/Avalicao.java
+++ api/src/main/java/com/mconnect/api/domain/Avaliacao/Avalicao.java
@@ -1,35 +1,37 @@
-package com.mconnect.api.domain.contratante;
+package com.mconnect.api.domain.Avaliacao;
 
 import java.util.UUID;
+
 import com.mconnect.api.domain.Usuario.Usuario;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "contratante")
 @Entity
+@Table(name="avaliacao")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Contratante {
+public class Avalicao {
 
     @Id
     @GeneratedValue
     private UUID id;
 
-    private String organizacao;
+    private Integer nota;
+    private String desc_avaliacao;
 
-    @OneToOne
+    @OneToMany
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
+    
 }
--- api/src/main/java/com/mconnect/api/domain/Usuario/Usuario.java
+++ api/src/main/java/com/mconnect/api/domain/Usuario/Usuario.java
@@ -1,35 +1,35 @@
-package com.mconnect.api.domain.contratante;
+package com.mconnect.api.domain.Usuario;
+
 
 import java.util.UUID;
-import com.mconnect.api.domain.Usuario.Usuario;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table(name = "contratante")
 @Entity
-@Getter
+@Table(name = "usuario")
 @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Contratante {
-
+public class Usuario {
     @Id
     @GeneratedValue
     private UUID id;
 
-    private String organizacao;
+    private String email;
+    private String usuario;
+    private String senha;
+    private String local_atuacao;
+    private String imagem_perfil_url;
+    private String banner_url;
+    private String descricao;
+    private String tipo_usuario;
 
-    @OneToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
 }
